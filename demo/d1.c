792de516da13d7f72c0d5ffc8550c21ae7d6b759
Amol Surati <asurati@users.noreply.github.com>
2021-08-26 15:23:25 +0530

    d1: The QAS assembler now use acc0/r0 as the default sources instead of RF_AB.39 register. As a result, the generated code changes. Update it for d1 demo.

--- demo/d1.c
+++ demo/d1.c
@@ -12,10 +12,10 @@ int d1_run()
 {
 	int err;
 	static const uint32_t code[] __attribute__((aligned(8))) = {
-		0x159c1ff6, 0xd00209a7,
-		0x009e7db6, 0x300009e7,
-		0x009e7db6, 0x100009e7,
-		0x009e7db6, 0x100009e7,
+		0x159c1fc0, 0xd00209a7,
+		0x009e7000, 0x300009e7,
+		0x009e7000, 0x100009e7,
+		0x009e7000, 0x100009e7,
 	};
 	err = v3d_run_prog(va_to_pa((va_t)code), 0, 0);
 	return err;
