acebc09dec8aaf03fb36a2712962732cc9fb72b0
Amol Surati <asurati@users.noreply.github.com>
2021-08-27 10:45:08 +0530

    d2: Improve the QPU code by making use of branches to form loops.

--- demo/d2.c
+++ demo/d2.c
@@ -68,29 +68,26 @@ int d2_run()
 		0x159e7240, 0x10020ca7,
 		0x15ca7c00, 0x100209e7,
 
-		0x15800dc0, 0xd0020827,
-		0x15800dc0, 0xd0020867,
-		0x159e7000, 0x10021c67,
-		0x159e7240, 0x10021ca7,
-		0x159f2e00, 0x100209e7,
+		0x00000004, 0xe00248e7,
+		0x00000000, 0xe00248a7,
+
+		0x0d9e7680, 0x10022827,
+		0x00000050, 0xf02809e7,
+		0x009e7000, 0x100009e7,
+		0x009e7000, 0x100009e7,
+		0x009e7000, 0x100009e7,
 
 		0x15800dc0, 0xd0020827,
 		0x15800dc0, 0xd0020867,
 		0x159e7000, 0x10021c67,
 		0x159e7240, 0x10021ca7,
 		0x159f2e00, 0x100209e7,
+		0x0c9c15c0, 0xd00208a7,
 
-		0x15800dc0, 0xd0020827,
-		0x15800dc0, 0xd0020867,
-		0x159e7000, 0x10021c67,
-		0x159e7240, 0x10021ca7,
-		0x159f2e00, 0x100209e7,
-
-		0x15800dc0, 0xd0020827,
-		0x15800dc0, 0xd0020867,
-		0x159e7000, 0x10021c67,
-		0x159e7240, 0x10021ca7,
-		0x159f2e00, 0x100209e7,
+		0xffffff88, 0xf0f809e7,
+		0x009e7000, 0x100009e7,
+		0x009e7000, 0x100009e7,
+		0x009e7000, 0x100009e7,
 
 		0x159c1fc0, 0xd00209a7,
 		0x009e7000, 0x300009e7,
@@ -135,7 +132,6 @@ int d2_run()
 		con_out("d2: [%d]: %x, %x", i, in_buf[i], out_buf[i]);
 	return err;
 }
-
 #if 0
 ori	r0, uni_rd, 0;
 ori	r1, uni_rd, 0;
@@ -143,30 +139,26 @@ or	vpm_rd_setup, r0, r0;
 or	vpm_ld_addr, r1, r1;
 or	-, vpm_ld_wait, r0;
 
+#r2 is the index
+#r3 is the limit
+
+li	r3, -, 4;
+li	r2, -, 0;
+
+loop:
+sub	r0, r3, r2	sf;
+b.z	done;
+;;;
 ori	r0, uni_rd, 0;
 ori	r1, uni_rd, 0;
 or	vpm_wr_setup, r0, r0;
 or	vpm_st_addr, r1, r1;
 or	-, vpm_st_wait, r0;
+addi	r2, r2, 1;
+b	loop;
+;;;
 
-ori	r0, uni_rd, 0;
-ori	r1, uni_rd, 0;
-or	vpm_wr_setup, r0, r0;
-or	vpm_st_addr, r1, r1;
-or	-, vpm_st_wait, r0;
-
-ori	r0, uni_rd, 0;
-ori	r1, uni_rd, 0;
-or	vpm_wr_setup, r0, r0;
-or	vpm_st_addr, r1, r1;
-or	-, vpm_st_wait, r0;
-
-ori	r0, uni_rd, 0;
-ori	r1, uni_rd, 0;
-or	vpm_wr_setup, r0, r0;
-or	vpm_st_addr, r1, r1;
-or	-, vpm_st_wait, r0;
-
+done:
 ori	host_int, 1, 1;
 pe;;;
 #endif
