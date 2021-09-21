8f3d2e605f200f429cc800f06c9878798b17aeef
Amol Surati <asurati@users.noreply.github.com>
2021-09-21 15:46:34 +0530

    slabs: Simplify to remove dependency upon divmod function. Rename to plural.

--- sys/slabs.c
+++ sys/slabs.c
@@ -15,10 +15,26 @@
 #include <sys/mutex.h>
 #include <sys/slab.h>
 
-// Max # of objects in any 64KB page is 64KB / 8 = 8192 = 0x2000
-// A single 64KB bitmap can handle 32GB of area.
+// Max # of objects in any 64KB page is 64KB / 8 = 8192 = 0x2000. This value
+// can fit inside a short.
 
-#define SLAB_ENTRY			0xcafe
+// The PAGE_SIZE is 64KB.
+// On 64bit archs: SLABS_SIZE == 32GB, sizeof(slab_entry) == 24 bytes
+// On 32bit archs: SLABS_SIZE == 512MB, sizeof(slab_entry) == 20 bytes
+//
+// On 64bit archs:
+// SLAB_SIZE == 32GB. # of pages is 1ul << (35 - 16) = 0x80000.
+// We need 0x80000 slab_entries. They amount to 0x80000 * 24 = 0xc00000 bytes,
+// or 0xc0 pages. The first 0xc0 pages, [0, 0xc0) of the SLABS area are
+// reserved for allocating slab_entries.
+//
+// On 32bit archs:
+// SLAB_SIZE == 512MB. # of pages is 1ul << (29 - 16) = 0x2000.
+// We need 0x2000 slab_entries. They amount to 0x2000 * 20 = 0x28000 bytes.
+// Align up to PAGE_SIZE gives 0x30000, or 3 pages. The first 3 pages,
+// [0, 3) of the SLABS area are reserved for allocating slab_entries.
+
+#define SLAB_ENTRY			0x51ab
 
 struct slab {
 	struct list_head		free_head;
@@ -26,12 +42,11 @@ struct slab {
 	struct mutex			lock;
 	struct cond_var			wait;
 	int				num_free;
-	short				num_objs_per_entry;
+	short				num_objs_per_page;
 	unsigned short			flags;
 };
 
 // Each entry handles 1 64KB page.
-// sizeof on 64-bit: 32 bytes.
 struct slab_entry {
 	unsigned short			flags;
 	unsigned short			slab_ix;
@@ -44,16 +59,24 @@ struct slab_entry {
 #define NUM_SE_PER_PAGE			(PAGE_SIZE / sizeof(struct slab_entry))
 #define NUM_SLAB_SIZES			13
 
+// [3, 16)
+#define SLAB_LOG2_START_SIZE		3
+#define SLAB_LOG2_END_SIZE		(SLAB_LOG2_START_SIZE + NUM_SLAB_SIZES)
+
+#define SLAB_OBJ_SIZE(ix)		(1 << (SLAB_LOG2_START_SIZE + ix))
+
 struct slabs_manager {
 	struct mutex			lock;
-	vpn_t				base_pages[2];
+	vpn_t				base_page;
+	vpn_t				base_page_obj;
 	int				num_pages;
 
 	struct bitmap			map;
 	struct slab			slabs[NUM_SLAB_SIZES];
 };
 
-static const size_t g_slab_sizes[NUM_SLAB_SIZES] = {
+#if 0
+static const size_t g_log2_slab_sizes[NUM_SLAB_SIZES] = {
 	1ul << 3,	// 8
 	1ul << 4,	// 16
 	1ul << 5,	// 32
@@ -68,11 +91,13 @@ static const size_t g_slab_sizes[NUM_SLAB_SIZES] = {
 	1ul << 14,	// 16384
 	1ul << 15,	// 32768
 };
+#endif
+
 static struct slabs_manager g_sm;
 
 // Called at IPL_THREAD
 static
-int slab_se_init(unsigned short slab_ix, struct slab_entry *se, vpn_t page,
+int slab_se_init(int slab_ix, struct slab_entry *se, vpn_t page,
 		 pfn_t frame)
 {
 	int i;
@@ -95,8 +120,8 @@ int slab_se_init(unsigned short slab_ix, struct slab_entry *se, vpn_t page,
 	se->flags = SLAB_ENTRY;
 	se->frame = frame;
 	se->slab_ix = slab_ix;
-	se->num_free = slab->num_objs_per_entry;
-	assert(se->num_free >= 0);
+	se->num_free = slab->num_objs_per_page;
+	assert(se->num_free > 0);
 	se->next_free = 0;
 
 	p = (char *)vpn_to_va(page);
@@ -104,7 +129,7 @@ int slab_se_init(unsigned short slab_ix, struct slab_entry *se, vpn_t page,
 	// Initialize the linked list.
 	for (i = 0; i < se->num_free - 1; ++i) {
 		*(short *)p = i + 1;
-		p += g_slab_sizes[slab_ix];
+		p += SLAB_OBJ_SIZE(slab_ix);
 	}
 	*(short *)p = -1;	// Terminate the linked list.
 
@@ -114,26 +139,21 @@ int slab_se_init(unsigned short slab_ix, struct slab_entry *se, vpn_t page,
 	slab->flags &= ~1;
 	cond_var_signal(&slab->wait);
 	mutex_unlock(&slab->lock);
-
 	return ERR_SUCCESS;
 }
 
-// # of pages in 32GB is 524288 = 0x80000. Thus we need 0x80000 slab_entries
-// They consume 0x80000*24 = 0xc00000 bytes of memory, or 192 pages.
-// So the first 192 pages of the 32GB area are kept reserved for allocating
-// slab_entries.
 
 // Called at IPL_THREAD
 int slabs_init(va_t *sys_end)
 {
-	int i, err;
+	int i, err, j;
 	size_t size;
 	struct slab *slab;
-	static char bits[PAGE_SIZE]
-		__attribute__((aligned(PAGE_SIZE))) = {0};
+	static char bits[SLABS_SIZE >> (PAGE_SIZE_BITS + 3)]
+		__attribute__((aligned(8))) = {0};
 
 	mutex_init(&g_sm.lock);
-	g_sm.base_pages[0] = va_to_vpn(SLABS_BASE);
+	g_sm.base_page = va_to_vpn(SLABS_BASE);
 	g_sm.num_pages = SLABS_SIZE >> PAGE_SIZE_BITS;
 	err = bitmap_init(&g_sm.map, (void *)bits, g_sm.num_pages);
 	if (err)
@@ -145,25 +165,26 @@ int slabs_init(va_t *sys_end)
 		cond_var_init(&slab->wait);
 		list_init(&slab->free_head);
 		list_init(&slab->part_head);
-		slab->num_objs_per_entry =
-			divmod(PAGE_SIZE, g_slab_sizes[i], NULL);
+
+		j = PAGE_SIZE_BITS - (SLAB_LOG2_START_SIZE + i);
+		slab->num_objs_per_page = 1 << j;
 	}
 
 	// total # of slab_entries == num_pages.
 
 	size = g_sm.num_pages * sizeof(struct slab_entry);
 	size = align_up(size, PAGE_SIZE_BITS);
-	g_sm.base_pages[1] = g_sm.base_pages[0] + (size >> PAGE_SIZE_BITS);
+	g_sm.base_page_obj = g_sm.base_page + (size >> PAGE_SIZE_BITS);
 	return ERR_SUCCESS;
 	(void)sys_end;
 }
 
 static
-int slabs_ptr_to_se(void *ptr, struct slab_entry **out_se, int *out_obj)
+int slabs_ptr_to_se(void *ptr, struct slab_entry **out_se, int *out_obj_ix)
 {
 	va_t va;
-	int obj, err, page_ix, se_page_ix, ix;
-	unsigned short slab_ix;
+	int obj_ix, err, page_ix, se_page_ix, ix, slab_entry_ix;
+	int slab_ix;
 	vpn_t page;
 	char *p;
 	struct slab *slab;
@@ -176,14 +197,15 @@ int slabs_ptr_to_se(void *ptr, struct slab_entry **out_se, int *out_obj)
 	page = va_to_vpn(va);
 
 	// The allocation must be within the data region.
-	if (page < g_sm.base_pages[1] || page >= g_sm.base_pages[0] +
+	if (page < g_sm.base_page_obj || page >= g_sm.base_page +
 	    g_sm.num_pages)
 		return ERR_PARAM;
 
-	page_ix = page - g_sm.base_pages[0];
-	se_page_ix = page_ix / NUM_SE_PER_PAGE;
-	se = (struct slab_entry *)vpn_to_va(g_sm.base_pages[0] + se_page_ix);
-	ix = page_ix - se_page_ix * NUM_SE_PER_PAGE;
+	slab_entry_ix = page_ix = page - g_sm.base_page;
+	se_page_ix = (slab_entry_ix * sizeof(struct slab_entry))
+		>> PAGE_SIZE_BITS;
+	se = (struct slab_entry *)vpn_to_va(g_sm.base_page + se_page_ix);
+	ix = slab_entry_ix - se_page_ix * NUM_SE_PER_PAGE;
 	se += ix;
 
 	// The page and the corresponding se_page should be allocated.
@@ -201,18 +223,20 @@ int slabs_ptr_to_se(void *ptr, struct slab_entry **out_se, int *out_obj)
 	mutex_unlock(&g_sm.lock);
 
 	slab_ix = se->slab_ix;
+	assert(slab_ix >= 0 && slab_ix < NUM_SLAB_SIZES);
 	slab = &g_sm.slabs[slab_ix];
 
+	// Check alignment.
 	err = ERR_PARAM;
 	mutex_lock(&slab->lock);
-	// Check alignment.
 	p = (char *)vpn_to_va(page);
-	obj = divmod((char *)ptr - p, g_slab_sizes[slab_ix], NULL);
-	if (ptr != p + obj * g_slab_sizes[slab_ix])
+	// obj_ix = (ptr - p) / obj_size;
+	obj_ix = ((char *)ptr - p) >> (SLAB_LOG2_START_SIZE + slab_ix);
+	if (ptr != p + obj_ix * SLAB_OBJ_SIZE(slab_ix))
 		goto err1;
 	*out_se = se;
-	if (out_obj)
-		*out_obj = obj;
+	if (out_obj_ix)
+		*out_obj_ix = obj_ix;
 	// Returns with slab_lock held.
 	return ERR_SUCCESS;
 err1:
@@ -228,12 +252,12 @@ err0:
 static
 int slabs_free(void *ptr)
 {
-	int obj, err;
-	unsigned short slab_ix;
+	int obj_ix, err;
+	int slab_ix;
 	struct slab *slab;
 	struct slab_entry *se;
 
-	err = slabs_ptr_to_se(ptr, &se, &obj);
+	err = slabs_ptr_to_se(ptr, &se, &obj_ix);
 	if (err)
 		return err;
 
@@ -241,16 +265,16 @@ int slabs_free(void *ptr)
 	slab_ix = se->slab_ix;
 	slab = &g_sm.slabs[slab_ix];
 
-	memset(ptr, 0xff, g_slab_sizes[slab_ix]);
+	memset(ptr, 0xff, SLAB_OBJ_SIZE(slab_ix));
 
 	*(short *)ptr = se->next_free;
-	se->next_free = obj;
+	se->next_free = obj_ix;
 	++se->num_free;
 	++slab->num_free;
 
 	if (se->num_free == 1) {
 		list_add_tail(&slab->part_head, &se->entry);
-	} else if (se->num_free == slab->num_objs_per_entry) {
+	} else if (se->num_free == slab->num_objs_per_page) {
 		list_del_entry(&se->entry);
 		list_add_tail(&slab->free_head, &se->entry);
 	}
@@ -274,31 +298,33 @@ int slabs_va_to_pa(void *ptr, pa_t *out)
 		return err;
 
 	slab = &g_sm.slabs[se->slab_ix];
+	pa = pfn_to_pa(se->frame);
+	mutex_unlock(&slab->lock);
 
 	va = (va_t)ptr;
-	pa = pfn_to_pa(se->frame);
 	pa |= va & (PAGE_SIZE - 1);
-	mutex_unlock(&slab->lock);
 	*out = pa;
 	return ERR_SUCCESS;
 }
 
 static
-int slab_se_alloc(int page_ix, struct slab_entry **out)
+int slab_se_alloc(int slab_entry_ix, struct slab_entry **out)
 {
 	int se_page_ix, err, ix;
 	vpn_t se_page;
 	struct slab_entry *se;
 	pfn_t se_frame;
 
-	// page_ix == slab_entry index.
-	se_page_ix = page_ix / NUM_SE_PER_PAGE;
-	se_page = g_sm.base_pages[0] + se_page_ix;
+	//se_page_ix = slab_entry_ix / NUM_SE_PER_PAGE;
+	se_page_ix = (slab_entry_ix * sizeof(struct slab_entry))
+		>> PAGE_SIZE_BITS;
+	se_page = g_sm.base_page + se_page_ix;
 
 	se = (struct slab_entry *)vpn_to_va(se_page);
 
 	mutex_lock(&g_sm.lock);
 	err = bitmap_is_off(&g_sm.map, se_page_ix, 1);
+
 	// The only (true) error we expect is ERR_UNEXP, which is sent when the
 	// se is found to be already allocated. Note we do not free the se's
 	// yet.
@@ -320,8 +346,9 @@ int slab_se_alloc(int page_ix, struct slab_entry **out)
 	}
 
 	err = ERR_SUCCESS;
-	// slab_entry index within the se_page == page_ix % NUM_SE_PER_PAGE;
-	ix = page_ix - se_page_ix * NUM_SE_PER_PAGE;
+	// slab_entry index within the se_page is
+	// slab_entry_ix % NUM_SE_PER_PAGE;
+	ix = slab_entry_ix - se_page_ix * NUM_SE_PER_PAGE;
 	se += ix;
 	*out = se;
 	goto err0;
@@ -336,7 +363,7 @@ err0:
 
 // Called at IPL_THREAD
 static
-int slab_add_page(unsigned short slab_ix)
+int slab_add_page(int slab_ix)
 {
 	vpn_t page;
 	pfn_t frame;
@@ -348,8 +375,9 @@ int slab_add_page(unsigned short slab_ix)
 	if (slab_ix >= NUM_SLAB_SIZES)
 		return ERR_PARAM;
 
-	// Allocate a page for the data.
-	page_ix = g_sm.base_pages[1] - g_sm.base_pages[0];
+	// Allocate a page for the data. Search for an empty page in the
+	// SLABS obj area.
+	page_ix = g_sm.base_page_obj - g_sm.base_page;
 	mutex_lock(&g_sm.lock);
 	err = page_ix = bitmap_find_off(&g_sm.map, 0, page_ix, 1);
 	if (page_ix < 0) {
@@ -361,8 +389,6 @@ int slab_add_page(unsigned short slab_ix)
 	if (err)
 		return err;
 
-	page = g_sm.base_pages[0] + page_ix;
-
 	// The page_ix is reserved. Allocate the corresponding slab_entry.
 	se = NULL;
 	err = slab_se_alloc(page_ix, &se);
@@ -373,6 +399,8 @@ int slab_add_page(unsigned short slab_ix)
 	err = pmm_alloc(ALIGN_PAGE, 1, &frame);
 	if (err)
 		goto err1;
+
+	page = g_sm.base_page + page_ix;
 	err = mmu_map_page(0, page, frame, ALIGN_PAGE, PROT_RW);
 	if (err)
 		goto err2;
@@ -394,9 +422,18 @@ err0:
 	return err;
 }
 
+static
+int se_to_ix(const struct slab_entry *se)
+{
+	va_t va;
+
+	va = vpn_to_va(g_sm.base_page);
+	return se - (const struct slab_entry *)va;
+}
+
 // Called at IPL_THREAD
 static
-int slab_alloc_locked(unsigned short slab_ix, void **out)
+int slab_alloc_locked(int slab_ix, void **out)
 {
 	char *p;
 	vpn_t page;
@@ -418,11 +455,10 @@ int slab_alloc_locked(unsigned short slab_ix, void **out)
 		assert(0);	// Should not occur.
 
 	se = list_entry(head->next, struct slab_entry, entry);
-	slab_entry_ix =
-		se - (struct slab_entry *)vpn_to_va(g_sm.base_pages[0]);
-	page = g_sm.base_pages[0] + slab_entry_ix;
+	slab_entry_ix = se_to_ix(se);
+	page = g_sm.base_page + slab_entry_ix;
 
-	off = se->next_free * g_slab_sizes[slab_ix];
+	off = se->next_free * SLAB_OBJ_SIZE(slab_ix);
 	p = (char *)vpn_to_va(page);
 	data = (short *)(p + off);
 	se->next_free = *data;
@@ -431,18 +467,18 @@ int slab_alloc_locked(unsigned short slab_ix, void **out)
 
 	if (se->num_free == 0) {
 		list_del_entry(&se->entry);
-	} else if (se->num_free == slab->num_objs_per_entry - 1) {
+	} else if (se->num_free == slab->num_objs_per_page - 1) {
 		list_del_entry(&se->entry);
 		list_add_tail(&slab->part_head, &se->entry);
 	}
-	memset(data, 0xff, g_slab_sizes[slab_ix]);
+	memset(data, 0xff, SLAB_OBJ_SIZE(slab_ix));
 	*out = data;
 	return ERR_SUCCESS;
 }
 
 // Called at IPL_THREAD
 static
-int slab_alloc(unsigned short slab_ix, void **out)
+int slab_alloc(int slab_ix, void **out)
 {
 	int err;
 	struct slab *slab;
@@ -488,12 +524,13 @@ int slab_alloc(unsigned short slab_ix, void **out)
 
 void *malloc(size_t size)
 {
-	int i, err;
+	int i, j, err;
 	void *out;
 
-	for (i = 0; i < NUM_SLAB_SIZES; ++i)
-		if (size <= g_slab_sizes[i])
+	for (i = 0, j = SLAB_LOG2_START_SIZE; i < NUM_SLAB_SIZES; ++i, ++j) {
+		if (size <= (1ul << j))
 			break;
+	}
 
 	assert(i < NUM_SLAB_SIZES);
 	if (i == NUM_SLAB_SIZES)
