093d3eb3c0d29be177fbe30b4f2321b3c51461a0
Amol Surati <asurati@users.noreply.github.com>
2021-09-29 09:38:57 +0530

    d50: Place the alpha component in the same manner as other components are placed - by utilizing MUL unit pack facility.

--- demo/d50.c
+++ demo/d50.c
@@ -152,17 +152,20 @@ int d50_run()
 	static const uint32_t code[] __attribute__((aligned(8))) = {
 		0x203e303e, 0x100049e0, // fmul r0, vary_rd, a15;
 		0x019e7140, 0x10020827, // fadd r0, r0, r5;
+
 		0x203e303e, 0x100049e1, // fmul r1, vary_rd, a15;
 		0x019e7340, 0x10020867, // fadd r1, r1, r5;
+
 		0x203e303e, 0x100049e2, // fmul r2, vary_rd, a15;
 		0x019e7540, 0x100208a7, // fadd r2, r2, r5;
+
+		0x000000ff, 0xe00208e7, // li   r3, -, 0xff;
 
 		0x209e0007, 0xd16049e0, // fmuli        r0, r0, 1f      pm8c;
 		0x209e000f, 0xd15049e0, // fmuli        r0, r1, 1f      pm8b;
 		0x209e0017, 0xd14049e0, // fmuli        r0, r2, 1f      pm8a;
+		0x209e001f, 0xd17049e0, // fmuli        r0, r3, 1f      pm8d;
 
-		0xff000000, 0xe0020867, // li   r1, -, 0xff000000;
-		0x159e7040, 0x10020827, // or   r0, r0, r1;
 		0x159e7000, 0x50020ba7, // or   tlb_clr_all, r0, r0     usb;
 
 		0x159c1fc0, 0xd00209a7, // ori  host_int, 1, 1;
@@ -331,6 +334,7 @@ int d50_run()
 
 	v3d_run_renderer(va_to_ba((va_t)v3dcr), off);
 
+	return -1;
 	return ERR_SUCCESS;
 }
 
@@ -347,6 +351,8 @@ fadd	r1, r1, r5;
 fmul	r2, vary_rd, a15;
 fadd	r2, r2, r5;
 
+li	r3, -, 0xff;	# alpha
+
 # Utilize MUL-pack facility to convert colour components from float to
 # byte with saturation, and place them at appropriate locations depending on
 # the framebuffer format. The format is 0x8d8c8b8a, corresponding to
@@ -354,9 +360,7 @@ fadd	r2, r2, r5;
 fmuli	r0, r0, 1f	pm8c;
 fmuli	r0, r1, 1f	pm8b;
 fmuli	r0, r2, 1f	pm8a;
-
-li	r1, -, 0xff000000;
-or	r0, r0, r1;
+fmuli	r0, r3, 1f	pm8d;
 
 or	tlb_clr_all, r0, r0	usb;
 
