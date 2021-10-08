7955885e4c7ac6921818e8fdfc450b20b5ec600d
Amol Surati <asurati@users.noreply.github.com>
2021-10-08 16:49:16 +0530

    demo: Calculate the number of demos automatically. Add the new demo d55.

--- demo/demo.c
+++ demo/demo.c
@@ -3,8 +3,6 @@
 
 #include <dev/con.h>
 
-#define NUM_DEMOS			9
-
 int demo_run()
 {
 	typedef int (*fn_demo_run)();
@@ -18,17 +16,19 @@ int demo_run()
 	int	d52_run();
 	int	d53_run();
 	int	d54_run();
+	int	d55_run();
 
 	static const fn_demo_run fns[] = {
 		d1_run, d2_run, d3_run, d4_run, d50_run, d51_run, d52_run,
-		d53_run, d54_run,
+		d53_run, d54_run, d55_run,
 	};
 
 	static const char *fn_names[] = {
 		"d1", "d2", "d3", "d4", "d50", "d51", "d52", "d53", "d54",
+		"d55"
 	};
 
-	for (i = 0; i < NUM_DEMOS; ++i) {
+	for (i = 0; i < (int)(sizeof(fns)/sizeof(fns[0])); ++i) {
 		err = fns[i]();
 		con_out("%s: err %x", fn_names[i], err);
 		if (err)
