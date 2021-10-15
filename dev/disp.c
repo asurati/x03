9a911dc8317c91ae314cbe9b894ae5bc1bea5ae8
Amol Surati <asurati@users.noreply.github.com>
2021-10-15 09:56:55 +0530

    disp: Replace various constants with corresponding definitions. Allocate a framebuffer instead of using a hard-coded address. Comment out unused code sections.

--- dev/disp.c
+++ dev/disp.c
@@ -7,6 +7,7 @@
 #include <sys/err.h>
 #include <sys/cpu.h>
 #include <sys/vmm.h>
+#include <sys/pmm.h>
 
 #include <dev/dev.h>
 #include <dev/con.h>
@@ -29,6 +30,10 @@ volatile uint32_t *g_txp_regs;
 volatile uint32_t *g_hdmi_regs;
 volatile uint32_t *g_hd_regs;
 volatile uint32_t *g_ddc_regs;
+
+#define _1MB				(1024ul * 1024)
+
+static pa_t g_fb_pa;
 
 // Display Mode
 struct mode {
@@ -119,6 +124,7 @@ struct mode g_disp_modes[] = {
 #define VBP				8ul
 #endif
 
+#if 0
 // block is 0 or 1.
 static
 void read_edid_block(int block, uint8_t *buf)
@@ -188,6 +194,7 @@ void read_edid()
 	for (i = 0; i < 64; ++i)
 		con_out("0x%x,", p[i]);
 }
+#endif
 
 static
 void pv_disable()
@@ -211,10 +218,26 @@ void pv_enable()
 
 	pv_disable();
 
-	g_pv2_regs[PV_HORZA] = HSW | (HBP << 16);
-	g_pv2_regs[PV_HORZB] = HAP | (HFP << 16);
-	g_pv2_regs[PV_VERTA] = VSW | (VBP << 16);
-	g_pv2_regs[PV_VERTB] = VAL | (VFP << 16);
+	val = 0;
+	val |= bits_set(PV_HORZA_HSW, HSW);
+	val |= bits_set(PV_HORZA_HBP, HBP);
+	g_pv2_regs[PV_HORZA] = val;
+
+	val = 0;
+	val |= bits_set(PV_HORZB_HAP, HAP);
+	val |= bits_set(PV_HORZB_HFP, HFP);
+	g_pv2_regs[PV_HORZB] = val;
+
+	val = 0;
+	val |= bits_set(PV_VERTA_VSW, VSW);
+	val |= bits_set(PV_VERTA_VBP, VBP);
+	g_pv2_regs[PV_VERTA] = val;
+
+	val = 0;
+	val |= bits_set(PV_VERTB_VAL, VAL);
+	val |= bits_set(PV_VERTB_VFP, VFP);
+	g_pv2_regs[PV_VERTB] = val;
+
 	g_pv2_regs[PV_VID_CTRL] = bits_on(PV_VID_CTRL_CONTINUOUS);
 
 	val = 0;
@@ -222,19 +245,17 @@ void pv_enable()
 	val |= bits_on(PV_CTRL_CLR_AT_START);
 	val |= bits_on(PV_CTRL_TRIG_UFLOW);
 	val |= bits_on(PV_CTRL_WAIT_HSTART);
-	val |= (1 << 2);	// HDMI clock select.
-	val |= (0x2e << 15);
+	val |= bits_set(PV_CTRL_CLK_SEL, 1);	// HDMI clock select.
+	val |= bits_set(PV_CTRL_FIFO_LEVEL, 0x2e);	// Set by firmware.
 	g_pv2_regs[PV_CTRL] = val;
 
 	g_pv2_regs[PV_CTRL] |= bits_on(PV_CTRL_EN);
-	con_out("pve: %x", g_pv2_regs[PV_CTRL]);
 }
 
 static
 void pv_enable_video()
 {
 	g_pv2_regs[PV_VID_CTRL] |= bits_on(PV_VID_CTRL_EN);
-	con_out("pvev: %x", g_pv2_regs[PV_VID_CTRL]);
 }
 
 static
@@ -251,29 +272,17 @@ void hdmi_enable_video()
 		val |= bits_on(HDMI_VID_CTRL_NHSYNC);
 	if (VSP < 0)
 		val |= bits_on(HDMI_VID_CTRL_NVSYNC);
-#if 0
-	if (g_mode == 0) {
-		// vga has negative syncs.
-		val |= bits_on(HDMI_VID_CTRL_NHSYNC);
-		val |= bits_on(HDMI_VID_CTRL_NVSYNC);
-	}
-#endif
 	g_hd_regs[HDMI_VID_CTRL] = val;
-	//g_hd_regs[HDMI_VID_CTRL] &= bits_off(HDMI_VID_CTRL_BLANK);
 
-	// if we skip the sched ctrl and hdmi mode selection, the
-	// resoltuion is correct. so the problem does seem to be with
-	// infoframes.
-	goto do_hdmi;
-	goto skip_hdmi;
-do_hdmi:
+	// Set HDMI mode, and wait for it to become active.
+	g_hdmi_regs[HDMI_SCHED_CTRL] |= bits_on(HDMI_SCHED_CTRL_MODE_HDMI);
+	while (1) {
+		val = g_hdmi_regs[HDMI_SCHED_CTRL];
+		if (bits_get(val, HDMI_SCHED_CTRL_HDMI_ACT))
+			break;
+	}
 
-	g_hdmi_regs[HDMI_SCHED_CTRL] |= 1;	// contorl mode hdmi
-	while (!(g_hdmi_regs[HDMI_SCHED_CTRL] & (1 << 1)))
-		;
-
-	//g_hdmi_regs[HDMI_SCHED_CTRL] |= 1 << 3;	// alwasy keppout
-	g_hdmi_regs[HDMI_RAM_PKT_CFG] = 1 << 16;
+	g_hdmi_regs[HDMI_RAM_PKT_CFG] = bits_on(HDMI_RAM_PKT_CFG_EN);
 
 	// Write the AVI Infoframe
 	memset(&avi, 0, sizeof(avi));
@@ -283,13 +292,12 @@ do_hdmi:
 	avi.colour_scan = 0x10;
 	avi.vic = VIC;
 
-	//0x18 for vga, 0x28 for fhd
 	if (g_mode == 0 || g_mode == 1) {
 		//avi.colour_aspect = 0x98;
 		avi.colour_aspect = 0x18;	// 4:3
 	} else {
 		//avi.colour_aspect = 0xa8;
-		avi.colour_aspect = 0x28;
+		avi.colour_aspect = 0x28;	// 16:9
 	}
 
 	val = 0;
@@ -298,24 +306,27 @@ do_hdmi:
 		val += p[i];
 	avi.chksum = 0x100 - (uint8_t)val;
 
-	g_hdmi_regs[0x112] = p[0] | (p[1] << 8) | (p[2] << 16);
-	g_hdmi_regs[0x113] = p[3] | (p[4] << 8) | (p[5] << 16) | (p[6] << 24);
-	g_hdmi_regs[0x114] = p[7] | (p[8] << 8) | (p[9] << 16);
-	g_hdmi_regs[0x115] = p[10] | (p[11] << 8) | (p[12] << 16) | (p[13] << 24);
-	g_hdmi_regs[0x116] = p[14] | (p[15] << 8) | (p[16] << 16);
+	i = HDMI_RAM_PKT_START + (((0x82 - 0x80) * 0x24) >> 2);
+	g_hdmi_regs[i++] = p[0] | (p[1] << 8) | (p[2] << 16);
+	g_hdmi_regs[i++] = p[3] | (p[4] << 8) | (p[5] << 16) | (p[6] << 24);
+	g_hdmi_regs[i++] = p[7] | (p[8] << 8) | (p[9] << 16);
+	g_hdmi_regs[i++] = p[10] | (p[11] << 8) | (p[12] << 16) | (p[13] << 24);
+	g_hdmi_regs[i++] = p[14] | (p[15] << 8) | (p[16] << 16);
 
-	con_out("avi %x %x %x %x %x",
-		g_hdmi_regs[0x112],
-		g_hdmi_regs[0x113],
-		g_hdmi_regs[0x114],
-		g_hdmi_regs[0x115],
-		g_hdmi_regs[0x116]);
+	// Reload the packet. Without it, merely writing into the registers
+	// does not pass the infoframe packet to the display. As a result,
+	// the monitor receives bogus/empty AVI infoframe, and it displays a
+	// horizontal resolution of +2 pixels. For instance, if the desired
+	// resolution is 640x480@60Hz, the monitor displays 642x480@60Hz, with
+	// the left-most two columns of pixels a purple/violet colour.
+	// See https://bugs.freedesktop.org/show_bug.cgi?id=27452
 
+	// Load the AVI InfoFrame packet (index 2).
 	g_hdmi_regs[HDMI_RAM_PKT_CFG] |= 1 << 2;
 	while (!(g_hdmi_regs[HDMI_RAM_PKT_STS] & (1 << 2)))
 	       ;
+#if 0
 	goto recenter_fifo;
-
 skip_hdmi:
 	g_hdmi_regs[HDMI_RAM_PKT_CFG] &= ~(1 << 16);
 	g_hdmi_regs[HDMI_SCHED_CTRL] &= (~1);
@@ -334,15 +345,7 @@ recenter_fifo:
 	g_hdmi_regs[HDMI_FIFO_CTRL] = val | (1 << 6);
 	while (!(g_hdmi_regs[HDMI_FIFO_CTRL] & (1 << 14)))
 		;
-}
-
-static
-void hdmi_disable_audio()
-{
-	return;
-	g_hd_regs[HDMI_MAI_CTRL] = 1;
-	g_hd_regs[HDMI_MAI_CTRL] = 2;
-	g_hd_regs[HDMI_MAI_CTRL] = 1 << 9;
+#endif
 }
 
 static
@@ -396,7 +399,6 @@ void hdmi_disable()
 	// Disable PLLH.
 	g_cm_regs[CM_PLLH] = bits_on(CM_PLL_ANA_RESET) | CM_PASSWORD;
 	g_cm_regs[A2W_PLLH_CTRL] |= bits_on(A2W_PLL_CTRL_PWRDN) | CM_PASSWORD;
-	con_out("hdmi_dis: %x %x", g_cm_regs[CM_PLLH], g_cm_regs[A2W_PLLH_CTRL]);
 }
 
 static
@@ -441,113 +443,105 @@ void hdmi_enable()
 	//ndiv = 0x1c;
 	//fdiv = 0x33334;
 
-//	ndiv = 0x11;//0x10
-//	fdiv = 0x70000;	// 315.6MHz	locks successfully; 0x60000 doesn't
+	//ndiv = 0x10;
+	//fdiv = 0x70000;	// 315.6MHz	locks successfully; 0x60000 doesn't
 
 	ndiv = g_disp_modes[g_mode].pllh_ndiv;
 	fdiv = g_disp_modes[g_mode].pllh_fdiv;
-	con_out("hdmi_en");
 
-	g_cm_regs[A2W_XOCS_CTRL] |= 1 | CM_PASSWORD;
+	val = bits_on(A2W_XOCS_CTRL_PLLC_EN);
+	g_cm_regs[A2W_XOCS_CTRL] |= val | CM_PASSWORD;
 
-	con_out("xosc_ctrl %x", g_cm_regs[A2W_XOCS_CTRL]);
 	ana[0] = 0x900000;
 	ana[1] = 0xc;
 	ana[2] = 0;
 	ana[3] = 0;
 	for (i = 3; i >= 0; --i)
 		g_cm_regs[A2W_PLLH_ANA0 + i] = ana[i] | CM_PASSWORD;
-	con_out("wrote ana");
 
+	// Set PLLH Parameters.
 	g_cm_regs[A2W_PLLH_FRAC] = fdiv | CM_PASSWORD;
-	con_out("wrote frac");
 
 	val = g_cm_regs[A2W_PLLH_CTRL];
-	val &= ~0x3ff;
-	val &= ~0x7000;
-	val |= ndiv;
-	val |= 0x1000;	// pdiv.
+	val &= bits_off(A2W_PLL_CTRL_NDIV);
+	val &= bits_off(A2W_PLL_CTRL_PDIV);
+	val |= bits_set(A2W_PLL_CTRL_NDIV, ndiv);
+	val |= bits_set(A2W_PLL_CTRL_PDIV, 1);
 	g_cm_regs[A2W_PLLH_CTRL] = val | CM_PASSWORD;
-	con_out("wrote ndiv");
 
-	// PLLH_PIX rate. Change div to 2 to get (600MH/10)/2 = 30Mhz pixel clock
-	//g_cm_regs[A2W_PLLH_PIX] = 0x103 | CM_PASSWORD;
-	//g_cm_regs[A2W_PLLH_PIX] = 0x102 | CM_PASSWORD;
-	//g_cm_regs[A2W_PLLH_PIX] = 0x101 | CM_PASSWORD;
-	val = g_cm_regs[A2W_PLLH_PIX] & (~0xff);
-	val |= g_disp_modes[g_mode].pllh_pix_div;
+	// Set PLLH_PIX Parameters.
+	val = g_cm_regs[A2W_PLLH_PIX];
+	val &= bits_off(A2W_PLL_DIV);
+	val |= bits_set(A2W_PLL_DIV, g_disp_modes[g_mode].pllh_pix_div);
 	g_cm_regs[A2W_PLLH_PIX] = val | CM_PASSWORD;
 
-	con_out("pllh_pix.a2w_reg %x", g_cm_regs[A2W_PLLH_PIX]);
+	// Reload PLLH_PIX Parameters.
 	val = g_cm_regs[CM_PLLH];
 	g_cm_regs[CM_PLLH] = val | bits_on(CM_PLLH_LOAD_PIX) | CM_PASSWORD;
 	g_cm_regs[CM_PLLH] = (val & bits_off(CM_PLLH_LOAD_PIX)) | CM_PASSWORD;
 
-	// PLLH Enable.
+	// Enable PLLH.
 	val = g_cm_regs[A2W_PLLH_CTRL];
 	val &= bits_off(A2W_PLL_CTRL_PWRDN);
 	g_cm_regs[A2W_PLLH_CTRL] = val | CM_PASSWORD;
-	con_out("wrote ~pwrdn");
 
 	val = g_cm_regs[CM_PLLH];
 	val &= bits_off(CM_PLL_ANA_RESET);
 	g_cm_regs[CM_PLLH] = val | CM_PASSWORD;
-	con_out("wrote ~anarst %x %x %x", g_cm_regs[CM_PLLH],
-		g_cm_regs[A2W_PLLH_CTRL], g_cm_regs[CM_LOCK]);
 
 	while (!bits_get(g_cm_regs[CM_LOCK], CM_LOCK_FLOCKH))
 		;
-	con_out("got lock %x", g_cm_regs[CM_LOCK]);
 
 	val = bits_on(A2W_PLL_CTRL_PRST_DISABLE);
 	g_cm_regs[A2W_PLLH_CTRL] |= val | CM_PASSWORD;
-	con_out("wrote ~prst_dis");
 
-	// PLLH_PIX Enable.
+	// Enable PLLH_PIXe.
 	val = g_cm_regs[A2W_PLLH_PIX];
 	val &= bits_off(A2W_PLL_CHANNEL_DISABLE);
 	g_cm_regs[A2W_PLLH_PIX] = val | CM_PASSWORD;
-	con_out("wrote ~ch_dis %x", g_cm_regs[A2W_PLLH_PIX]);
 
 	hdmi_phy_enable();
-	con_out("wrote phy_en");
 
-	val = 0;
+	//val = 0;
 	//val |= bits_on(HDMI_SCHED_CTRL_MANUAL_FMT);
 	//val |= bits_on(HDMI_SCHED_CTRL_IGNORE_VP);
-	g_hdmi_regs[HDMI_SCHED_CTRL] |= val;
-	con_out("wrote sched");
+	//g_hdmi_regs[HDMI_SCHED_CTRL] |= val;
 
 	val = HAP;
 	if (HSP > 0)
-		val |= 1 << 13;	// H +ve
+		val |= bits_on(HDMI_HORZA_HPOS);	// HSync active high
 	if (VSP > 0)
-		val |= 1 << 14;	// V +ve
+		val |= bits_on(HDMI_HORZA_VPOS);	// VSync active high
 	g_hdmi_regs[HDMI_HORZA] = val;
-#if 0
-	if (g_mode == 1)
-		g_hdmi_regs[HDMI_HORZA] = HAP | 0x6000;	// 0x6000 for HV POSITIVE
-	else
-		g_hdmi_regs[HDMI_HORZA] = HAP;
-#endif
-	g_hdmi_regs[HDMI_HORZB] = HFP | (HSW << 10) | (HBP << 20);
-	con_out("wrote horz");
 
-	g_hdmi_regs[HDMI_VERTA0] =
-	g_hdmi_regs[HDMI_VERTA1] = VAL | (VFP << 13) | (VSW << 20);
+	val = 0;
+	val |= bits_set(HDMI_HORZB_HFP, HFP);
+	val |= bits_set(HDMI_HORZB_HSW, HSW);
+	val |= bits_set(HDMI_HORZB_HBP, HBP);
+	g_hdmi_regs[HDMI_HORZB] = val;
 
-	g_hdmi_regs[HDMI_VERTB0] =
+	val = 0;
+	val |= bits_set(HDMI_VERTA_VAL, VAL);
+	val |= bits_set(HDMI_VERTA_VFP, VFP);
+	val |= bits_set(HDMI_VERTA_VSW, VSW);
+	g_hdmi_regs[HDMI_VERTA0] = val;
+	g_hdmi_regs[HDMI_VERTA1] = val;
+
+	g_hdmi_regs[HDMI_VERTB0] = VBP;
 	g_hdmi_regs[HDMI_VERTB1] = VBP;
-	con_out("wrote vert");
 }
 
 static
 void hdmi_enable_csc_fifo()
 {
-	// csc setup. vga doesn't need conversoin, but anything else does.
+	uint32_t val;
 
-	g_hd_regs[HDMI_CSC_CTRL] = 0x2d;//2f in firmware
-	g_hdmi_regs[HDMI_FIFO_CTRL] = 1;
+	val = 0;
+	val |= bits_on(HDMI_CSC_CTRL_EN);
+	val |= bits_on(HDMI_CSC_CTRL_MODE);		// Custom mode.
+	val |= bits_set(HDMI_CSC_CTRL_ORDER, 1);	// BGR
+	g_hd_regs[HDMI_CSC_CTRL] = val;
+	g_hdmi_regs[HDMI_FIFO_CTRL] = bits_on(HDMI_FIFO_CTRL_MS_N);
 }
 
 static
@@ -559,7 +553,6 @@ void hvs_disable_channel()
 
 #define HVS_DL_MEM_INDEX		0x800
 #define HVS_DL_INDEX			0x400
-//volatile uint32_t fb[HAP * VAL];
 
 static
 void hvs_enable_channel()
@@ -593,8 +586,7 @@ void hvs_enable_channel()
 	dl[2] = val;
 	dl[3] = 0;
 
-	//dl[4] = va_to_ba((va_t)fb);
-	dl[4] = pa_to_ba(0x2000000);
+	dl[4] = pa_to_ba(g_fb_pa);
 	dl[5] = 0;
 	dl[6] = HAP * 4;	// Source Pitch, for Linear Tiling.
 	dl[7] = bits_on(HVS_DLW_CTL0_END);
@@ -612,6 +604,7 @@ void hvs_enable_channel()
 	g_hvs_regs[HVS_DL1_CTRL] = val;
 }
 
+#if 0
 static
 void dump_regs()
 {
@@ -632,15 +625,13 @@ void dump_regs()
 	for (i = 0; i < (0x2000 >> 2); ++i)
 		con_out("cm[%x]=%x", i, g_cm_regs[i]);
 }
+#endif
 
 // PV2 drives the HDMI encoder.
 int disp_config()
 {
-	//con_out("Before");
-	//dump_regs();
 	pv_disable_video();
 	hdmi_disable_video();
-	hdmi_disable_audio();
 	pv_disable();
 	hvs_disable_channel();
 	hdmi_disable();
@@ -652,9 +643,6 @@ int disp_config()
 	pv_enable_video();
 	hdmi_enable_video();
 	return ERR_SUCCESS;
-	con_out("After");
-	dump_regs();
-	return ERR_SUCCESS;
 }
 
 // IPL_THREAD
@@ -664,7 +652,7 @@ int disp_init()
 	int err, i, num_pages;
 	va_t va;
 	vpn_t pages, page;
-	pfn_t frame;
+	pfn_t frames, frame;
 
 	err = dev_map_io(HVS_BASE, 0x6000, &va);
 	if (err)
@@ -706,42 +694,58 @@ int disp_init()
 		return err;
 	g_ddc_regs = (volatile uint32_t *)va;
 
-	//num_pages = 19;		//VGA
-	num_pages = 127;	//FHD
-	// 640x480x4 == 19 64KB pages.
-	err = vmm_alloc(ALIGN_PAGE, num_pages, &pages);
+	// Allocate 8MB of system RAM for a single frame buffer.
+	// 1920*1080*4 = 8294400 < 8MB.
+
+	num_pages = (8 * _1MB) >> PAGE_SIZE_BITS;
+	err = pmm_alloc(ALIGN_1MB, num_pages, &frames);
 	if (err)
 		goto err0;
 
+	err = vmm_alloc(ALIGN_1MB, num_pages, &pages);
+	if (err)
+		goto err1;
+
 	page = pages;
-	frame = pa_to_pfn(0x2000000);
-	for (i = 0; i < num_pages; ++i, ++page, ++frame) {
-		err = mmu_map_page(0, page, frame, ALIGN_PAGE, PROT_RW);
+	frame = frames;
+	for (i = 0; i < 8; ++i) {
+		err = mmu_map_page(0, page, frame, ALIGN_1MB, PROT_RW);
 		if (err)
-			goto err1;
+			goto err2;
+
+		// Map in units of 1MB.
+		page += _1MB >> PAGE_SIZE_BITS;
+		frame += _1MB >> PAGE_SIZE_BITS;
 	}
+	g_fb_pa = pfn_to_pa(frames);
+
+	// Fill the frame buffer with red, with a white 1-pixel thick border.
 	fb = (volatile uint32_t *)vpn_to_va(pages);
 	for (i = 0; i < (int)(HAP * VAL); ++i)
 		fb[i] = 0xffff0000;
 
-	// first and last row
+	// First and Last row.
 	for (i = 0; i < (int)HAP; ++i) {
 		fb[i] = 0xffffffff;
 		fb[(VAL - 1) * HAP + i] = 0xffffffff;
 	}
 
-	// first and last column
+	// First and Last column.
 	for (i = 0; i < (int)VAL; ++i) {
 		fb[i * HAP] = 0xffffffff;
 		fb[i * HAP + (HAP - 1)] = 0xffffffff;
 	}
 
 	return ERR_SUCCESS;
-err1:
-	for (--i, --page; i >= 0; --i, --page)
+err2:
+	page -= _1MB >> PAGE_SIZE_BITS;
+	for (--i; i >= 0; --i) {
 		mmu_unmap_page(0, page);
-	vmm_free(pages, 19);
+		page -= _1MB >> PAGE_SIZE_BITS;
+	}
+	vmm_free(pages, num_pages);
+err1:
+	pmm_free(frames, num_pages);
 err0:
 	return err;
-	(void)read_edid;
 }
